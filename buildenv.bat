2c7e265bb333f7ddcbcd420f68f5fe2881e68aa6
Ger Moran <neomonkeus@gmail.com>
2012-10-10 14:08:03 +0100

    Added param check for user location of cscript.exe.

--- buildenv.bat
+++ buildenv.bat
@@ -14,6 +14,7 @@ set qt_path=C:\QtSDK
 set work_folder=%HOMEDRIVE%%HOMEPATH%
 
 set reg_exe=%HOMEDRIVE%\Windows\Systems32\reg.exe
+
 FOR /F "tokens=2*" %%A IN ('%reg_exe% QUERY "HKLM\SOFTWARE\Microsoft\VisualStudio\SxS\VC7" /v 9.0 2^> nul') do set _msvc2008=%%B
 if not "%_msvc2008%" == "" set compiler_type=msvc2008
 
@@ -133,7 +134,6 @@ rem ********************
 rem *** Architecture ***
 rem ********************
 
-echo.
 echo.Script Running:
 echo.
 echo.Setting Program Files
--- create-shortcuts.bat
+++ create-shortcuts.bat
@@ -1,15 +1,25 @@
 @echo off
 
+rem auto-detect sys32 folder
+if exist %HOMEDRIVE%\Windows\System32\cscript.exe (
 set cscript_path=%HOMEDRIVE%\Windows\System32\cscript.exe
-if exist %csript_path% goto runscript
-goto no_script
+echo.Found Cscript.exe
+goto run_script
+)
+
+rem check params
+if exist %1% ( 
+set cscript_path=%1%
+echo.Found Cscript.exe
+goto run_script
+)
+
+echo. Could not find Cscript.exe
+goto cleanup
 
 :run_script
 @for %%a in (ini\*.ini) do  shortcut.vbs "%HOMEDRIVE%%HOMEPATH%\Desktop\%%~na.lnk" "%comspec%" "/k %~dp0buildenv.bat %%~dfa"
-goto cleanup
-
-:no_script
-echo. Could not find Cscript.exe
+echo.Shortcuts created.
 goto cleanup
 
 :cleanup
