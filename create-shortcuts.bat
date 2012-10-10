6056fe1eb1f3a2afcb3091e3417823a737758fee
Ger Moran <neomonkeus@gmail.com>
2012-10-10 10:46:43 +0100

    Updated cscript path to point to System32 folder

--- create-shortcuts.bat
+++ create-shortcuts.bat
@@ -1,1 +1,1 @@
-@for %%a in (ini\*.ini) do  cscript shortcut.vbs "%HOMEDRIVE%%HOMEPATH%\Desktop\%%~na.lnk" "%comspec%" "/k %~dp0buildenv.bat %%~dfa"
+@for %%a in (ini\*.ini) do %HOMEDRIVE%\Windows\System32\cscript.exe shortcut.vbs "%HOMEDRIVE%%HOMEPATH%\Desktop\%%~na.lnk" "%comspec%" "/k %~dp0buildenv.bat %%~dfa"
