4b9b3922f0c281f38ebb86354a43593e67abb682
Ger Moran <neomonkeus@gmail.com>
2012-10-10 21:12:36 +0100

    Reverted %cscript% changes and removed call from .vbs script invokation so it runs correctly.

--- create-shortcuts.bat
+++ create-shortcuts.bat
@@ -1,26 +1,4 @@
 @echo off
 
-rem auto-detect sys32 folder
-if exist %HOMEDRIVE%\Windows\System32\cscript.exe (
-set cscript_path=%HOMEDRIVE%\Windows\System32\cscript.exe
-echo.Found Cscript.exe
-goto run_script
-)
-
-rem check params
-if exist "%1" ( 
-set cscript_path=%1
-echo.Found Cscript.exe
-goto run_script
-)
-
-echo. Could not find Cscript.exe
-goto cleanup
-
-:run_script
-@for %%a in (ini\*.ini) do %cscript_path% shortcut.vbs "%HOMEDRIVE%%HOMEPATH%\Desktop\%%~na.lnk" "%comspec%" "/k %~dp0buildenv.bat %%~dfa"
+@for %%a in (ini\*.ini) do shortcut.vbs "%HOMEDRIVE%%HOMEPATH%\Desktop\%%~na.lnk" "%comspec%" "/k %~dp0buildenv.bat %%~dfa"
 echo.Shortcuts created.
-goto cleanup
-
-:cleanup
-set cscript_path=
