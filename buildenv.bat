f264ba2d983b5d686f9766a3ed783949b292b250
Ger Moran <neomonkeus@gmail.com>
2012-10-16 14:36:40 +0100

    Dropped detection of Blender versions below 2.62 due to 2.62+ development targetting.

--- buildenv.bat
+++ buildenv.bat
@@ -207,7 +207,7 @@ if "%BLENDERHOME%" == "" (
   goto endblender
 )
 echo.Blender home: %BLENDERHOME%
-for %%A in (2.57,2.58,2.59,2.60,2.61,2.62,2.63,2.64) do (
+for %%A in (2.62,2.63,2.64, 2.65, 2.66,2.67) do (
   if exist "%BLENDERHOME%\%%A" set BLENDERVERSION=%%A
 )
 if "%BLENDERVERSION%" == "" (
