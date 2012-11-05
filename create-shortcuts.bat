1b45153f6e7e713f405d60964ea5b67f68aee50d
Ger Moran <neomonkeus@gmail.com>
2012-11-05 15:24:14 +0000

    Reverted commit to prefix "BuildEnv-" to generated shortcuts.

--- create-shortcuts.bat
+++ create-shortcuts.bat
@@ -1,4 +1,4 @@
 @echo off
 
-@for %%a in (ini\*.ini) do shortcut.vbs "%HOMEDRIVE%%HOMEPATH%\Desktop\Buildenv-%%~na.lnk" "%comspec%" "/k  %~dps0buildenv.bat %%~dfsa"
+@for %%a in (ini\*.ini) do shortcut.vbs "%HOMEDRIVE%%HOMEPATH%\Desktop\%%~na.lnk" "%comspec%" "/k  %~dps0buildenv.bat %%~dfsa"
 echo.Shortcuts created.
