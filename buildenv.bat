664a465c7ef666f0b4b6598920c6ddcfe81eec9a
Amorilia <amorilia@users.sourceforge.net>
2012-11-06 12:09:43 +0000

    Documentation fix.

--- buildenv.bat
+++ buildenv.bat
@@ -98,7 +98,7 @@ echo.  nsis=FOLDER             [default: %nsis_path%]
 
 echo.  cmake=FOLDER            [default: %_cmake%]
 
-echo.  7-zip=FOLDER            [default: %seven_zip%]
+echo.  seven_zip=FOLDER        [default: %seven_zip%]
 
 rem compilers
 echo.Compilers:
