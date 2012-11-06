ff234ba932b6c0218f7114e63c79414b79eedc94
Ger Moran <neomonkeus@gmail.com>
2012-11-06 10:28:44 +0000

    Unset and tidied up variables, print default values

--- create-shortcuts.bat
+++ create-shortcuts.bat
@@ -3,8 +3,10 @@
 :checkini
 if exist "%1" (
 set _ini_location=%1
+echo.Ini folder read-value [value=%_shortcut_folder%]
 goto checkoutfolder
 )
+
 set _ini_location=ini
 
 :checkoutfolder
@@ -25,11 +27,13 @@ goto error
 :createshortcut
 @for %%a in (%_ini_location%\*.ini) do shortcut.vbs "%_shortcut_folder%\%%~na.lnk" "%comspec%" "/k  %~dps0buildenv.bat %%~dfsa"
 echo.Shortcuts created.
+set _shortcut_folder=
+set _ini_location=
 goto eof
 
 :error
 echo.Could not locate scripts 
-echo.%_ini_location%
-echo.%_shortcut_folder%
+echo.Ini folder read-value [value=%_shortcut_folder%]
+echo.Shortcut read-value [value=%_shortcut_folder%]
 
 :eof
