f23eed5ab0e4928f4887f9fd8c8ea0c219bf1f1e
Ger Moran <neomonkeus@gmail.com>
2012-11-07 00:08:50 +0000

    Output corrected to use proper variable

--- create-shortcuts.bat
+++ create-shortcuts.bat
@@ -3,22 +3,22 @@
 :checkini
 if exist "%1" (
 set _ini_location=%1
-echo.Reading INI files from [folder=%_shortcut_folder%]
+echo.Reading .ini files from [folder=%_ini_location%]
 goto checkoutfolder
 )
 
 set _ini_location=ini
 
 :checkoutfolder
-if exist "%HOMEDRIVE%%HOMEPATH%\Desktop" (
+if exist "%HOMEDRIVE%%HOMEPATH%\Desktop" ( 
 set _shortcut_folder=%HOMEDRIVE%%HOMEPATH%\Desktop
-echo.Setting output folder [folder=%_shortcut_folder%]
+echo.Setting output folder [folder: %_shortcut_folder%]
 goto createshortcut
 )
 
 if exist "%2" (
 set _shortcut_folder=%2
-echo.Setting output folder [folder=%_shortcut_folder%]
+echo.Setting output folder [folder: %_shortcut_folder%]
 goto createshortcut
 )
 
@@ -27,18 +27,19 @@ goto error
 :createshortcut
 @for %%a in (%_ini_location%\*.ini) do shortcut.vbs "%_shortcut_folder%\%%~na.lnk" "%comspec%" "/k  %~dps0buildenv.bat %%~dfsa"
 echo.Shortcuts created.
-echo.INI files read from [folder=%_ini_location%]
-echo.Shortcut written to [folder=%_shortcut_folder%]
-set _shortcut_folder=
-set _ini_location=
+echo.INI files read from [folder: %_ini_location%]
+echo.Shortcut written to [folder: %_shortcut_folder%]
+
 goto eof
 
 :error
 echo.Unable not locate scripts.
 echo.Please pass correct path parameters: create-shortcuts.bat [ini source folder] [shortcut destination folder] 
 echo.
-echo.Attempted to read .ini files from [folder=%_ini_location%]
-echo.Attempted to write Shortcuts to [folder=%_shortcut_folder%]
+echo.Attempted to read .ini files from [folder: %_ini_location%]
+echo.Attempted to write Shortcuts to [folder: %_shortcut_folder%]
 echo.
 
 :eof
+set _shortcut_folder=
+set _ini_location=
