1a9f54706b8b6c8b7df270ac39bc4751861a553b
Ger Moran <neomonkeus@gmail.com>
2012-11-07 08:17:02 +0000

    Fix for not being able to set variable and echo due to bat syntax parser

--- create-shortcuts.bat
+++ create-shortcuts.bat
@@ -1,28 +1,35 @@
 @echo off
 
-:checkini
+rem Three scenarios, user pass 0, 1 or 2 params
+rem create-shortcuts.bat [ini source] [shortcut folder dest]
+
+:checkiniparam
 if exist "%1" (
-set _ini_location=%1
-echo.Reading .ini files from [folder=%_ini_location%]
+set _ini_location=%1 
+) else ( 
+set _ini_location=ini
+)
+echo.Setting input folder [folder: %_ini_location%]
+
+:checkshortcutparam
+if exist "%2" (
+set _shortcut_folder=%2
+) else (
 goto checkoutfolder
 )
-
-set _ini_location=ini
+echo.Setting output folder [folder: %_shortcut_folder%]
+goto createshortcut
 
 :checkoutfolder
 if exist "%HOMEDRIVE%%HOMEPATH%\Desktop" ( 
-set _shortcut_folder=%HOMEDRIVE%%HOMEPATH%\Desktop
-echo.Setting output folder [folder: %_shortcut_folder%]
-goto createshortcut
-)
-
-if exist "%2" (
-set _shortcut_folder=%2
-echo.Setting output folder [folder: %_shortcut_folder%]
-goto createshortcut
-)
-
+set _shortcut_folder=%HOMEDRIVE%%HOMEPATH%\Desktop 
+) else (
+echo. Could not find default Desktop location
 goto error
+) 
+
+echo.Setting output folder [folder: %_shortcut_folder%]
+goto createshortcut
 
 :createshortcut
 @for %%a in (%_ini_location%\*.ini) do shortcut.vbs "%_shortcut_folder%\%%~na.lnk" "%comspec%" "/k  %~dps0buildenv.bat %%~dfsa"
@@ -33,8 +40,9 @@ echo.Shortcut written to [folder: %_shortcut_folder%]
 goto eof
 
 :error
-echo.Unable not locate scripts.
-echo.Please pass correct path parameters: create-shortcuts.bat [ini source folder] [shortcut destination folder] 
+echo.Unable to locate scripts.
+echo.Please pass correct path additional parameters: 
+echo.create-shortcuts.bat [ini source folder] [shortcut destination folder] 
 echo.
 echo.Attempted to read .ini files from [folder: %_ini_location%]
 echo.Attempted to write Shortcuts to [folder: %_shortcut_folder%]
